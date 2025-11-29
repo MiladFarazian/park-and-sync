5691dab289ca253254837cabf7b72058118cb521
Milad Jon Farazian <miladfarazian@Milads-MacBook-Pro-2.local>
2025-11-28 18:03:38 -0800

    ios testing

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.miladfarazian.parkway',
+  appName: 'Parkway',
+  webDir: 'dist'
+};
+
+export default config;
